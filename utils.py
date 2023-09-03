724f9daea8929610d3ce1691a17fd53e971a85e2
bemarchant <bemarchant@uc.cl>
2023-09-03 11:09:23 -0300

    feature: minor changes in plot_rate_delay

--- utils.py
+++ utils.py
@@ -21,21 +21,28 @@ def which_period_day(date):
         return 'afternoon'
     return 'night'
 
-def plot_rate_delay(df, feature):
+def plot_rate_delay(df, features):
     
-    df = df.dropna(subset=[feature])
-    delay_counts = df[df['delay_15'] == 1].groupby(feature)['delay_15'].count()
-    on_time_counts = df[df['delay_15'] == 0].groupby(feature)['delay_15'].count()
-    df_counts = pd.DataFrame({'delay': delay_counts, 'on_time': on_time_counts})
-    df_counts['delay'] = df_counts['delay'].fillna(0)
-    df_counts['on_time'] = df_counts['on_time'].fillna(0)
-    df_counts['delay_ratio'] = df_counts['delay'] / (df_counts['delay'] + df_counts['on_time'])
+    n_features = len(features)
+    fig, axes = plt.subplots(n_features, 1, figsize=(10 * n_features, 5))
+    
+    if n_features == 1:
+        axes = [axes]  # Convert to a list if there's only one feature
+    for i, feature in enumerate(features):
+        delay_counts = df[df['delay_15'] == 1].groupby(feature)['delay_15'].count()
+        on_time_counts = df[df['delay_15'] == 0].groupby(feature)['delay_15'].count()
+        df_counts = pd.DataFrame({'delay': delay_counts, 'on_time': on_time_counts})
+        df_counts['delay'] = df_counts['delay'].fillna(0)
+        df_counts['on_time'] = df_counts['on_time'].fillna(0)
+        df_counts['delay_ratio'] = df_counts['delay'] / (df_counts['delay'] + df_counts['on_time'])
 
-    sns.pointplot(x = df_counts.index, y = df_counts['delay_ratio'], color='black')
-    plt.xticks(rotation=90)
-    plt.ylabel('Delay Ratio')
-    plt.grid('on')
-    plt.legend(loc='best')
-    plt.show()
+        sns.pointplot(x = df_counts.index, y = df_counts['delay_ratio'], color='black', ax=axes[i])
+        axes[i].set_xticklabels(axes[i].get_xticklabels(), rotation=90)
+        axes[i].set_ylabel('Delay Ratio')
+        axes[i].set_title(f'Feature: {feature}')
+        axes[i].grid(True)
     
+    plt.tight_layout()
+    plt.show()
+
     return
